9169780cbc63acd2f08bfdde5af9df07e0b08b62
chaozeyi <chaozeyi1995@sina.com>
2018-03-23 20:37:28 +0800

    278

--- src/algorithms/problem_278.java
+++ src/algorithms/problem_278.java
@@ -3,6 +3,14 @@ package algorithms;
 /**
  * Created by chaozeyi on 18/3/23.
  */
+
+/**
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+ * which causes all the following ones to be bad.
+
+ You are given an API bool isBadVersion(version) which will return whether version is bad.
+ Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ */
 public class problem_278 {
     public int firstBadVersion(int n) {
         return helper(1, n);
